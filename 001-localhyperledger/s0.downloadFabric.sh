48363823ec6b075d34cded395c7fc21407e7981e
Robert Kleniewski <robert.kleniewski@gmail.com>
2017-09-16 16:48:16 +0000

    new versions

--- 001-localhyperledger/s0.downloadFabric.sh
+++ 001-localhyperledger/s0.downloadFabric.sh
@@ -7,8 +7,15 @@ set -ev
 # DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Pull and tag the latest Hyperledger Fabric base image.
-docker pull hyperledger/fabric-peer:x86_64-1.0.0
-docker pull hyperledger/fabric-ca:x86_64-1.0.0
-docker pull hyperledger/fabric-ccenv:x86_64-1.0.0
-docker pull hyperledger/fabric-orderer:x86_64-1.0.0
-docker pull hyperledger/fabric-couchdb:x86_64-1.0.0
+docker pull hyperledger/fabric-peer:x86_64-1.0.2
+docker pull hyperledger/fabric-ca:x86_64-1.0.2
+docker pull hyperledger/fabric-ccenv:x86_64-1.0.2
+docker pull hyperledger/fabric-orderer:x86_64-1.0.2
+docker pull hyperledger/fabric-couchdb:x86_64-1.0.2
+
+# tag images
+docker tag hyperledger/fabric-peer:x86_64-1.0.2 hyperledger/fabric-peer:latest
+docker tag hyperledger/fabric-ca:x86_64-1.0.2 hyperledger/fabric-ca:latest
+docker tag hyperledger/fabric-ccenv:x86_64-1.0.2 hyperledger/fabric-ccenv:latest
+docker tag hyperledger/fabric-orderer:x86_64-1.0.2 hyperledger/fabric-orderer:latest
+docker tag hyperledger/fabric-couchdb:x86_64-1.0.2 hyperledger/fabric-couchdb:latest
